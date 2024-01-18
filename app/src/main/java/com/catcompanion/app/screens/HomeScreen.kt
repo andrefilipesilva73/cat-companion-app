4524386e876c99992c9f29f14dcff6ec69546d9a
André Filipe Silva <andrefilipesilva73@users.noreply.github.com>
2024-01-18 23:36:10 +0000

    Added a MediumTopAppBar to the Home Screen

--- app/src/main/java/com/catcompanion/app/screens/HomeScreen.kt
+++ app/src/main/java/com/catcompanion/app/screens/HomeScreen.kt
@@ -1,26 +1,45 @@
 package com.catcompanion.app.screens
 
 // Import necessary components from the Jetpack Compose library
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.catcompanion.app.R
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 // Annotate the function to opt into using ExperimentalMaterial3Api
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,12 +54,32 @@ fun Home(mainNavController: NavHostController, modifier: Modifier = Modifier) {
     // Get the current route from the back stack entry
     val currentRoute = currentBackStackEntry?.destination?.route
 
+    // Define Scroll behavior
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
     // Content goes here
     Scaffold(
+        modifier = Modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             // TopAppBar goes here
-            TopAppBar(
-                title = { Text("Hello Scaffold!") }
+            MediumTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
+                        NavigationBarDefaults.Elevation),
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        stringResource(id = R.string.home_title),
+                        // maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                },
+                scrollBehavior = scrollBehavior
             )
         },
         bottomBar = {
@@ -80,20 +119,25 @@ fun Home(mainNavController: NavHostController, modifier: Modifier = Modifier) {
             }
         }
     ) { innerPadding ->
-        // Content goes here, create the NavHost
-        NavHost(
-            navController = navController,
-            startDestination = "breedsListScreen",
-            modifier = Modifier.padding(innerPadding)
+        Surface(modifier = Modifier.fillMaxSize(), // Set the size to fill the maximum available space
+            color = MaterialTheme.colorScheme.surfaceColorAtElevation(NavigationBarDefaults.Elevation) // Set the background color from the MaterialTheme
         ) {
-            // Breeds List Screen
-            composable("breedsListScreen") {
-                BreedsListScreen(mainNavController)
-            }
+            // Content goes here, create the NavHost
+            NavHost(
+                navController = navController,
+                startDestination = "breedsListScreen",
+                modifier = Modifier
+                    .padding(innerPadding)
+            ) {
+                // Breeds List Screen
+                composable("breedsListScreen") {
+                    BreedsListScreen(mainNavController)
+                }
 
-            // Favorites List Screen
-            composable("favoritesListScreen") {
-                FavoritesListScreen(mainNavController)
+                // Favorites List Screen
+                composable("favoritesListScreen") {
+                    FavoritesListScreen(mainNavController)
+                }
             }
         }
     }
