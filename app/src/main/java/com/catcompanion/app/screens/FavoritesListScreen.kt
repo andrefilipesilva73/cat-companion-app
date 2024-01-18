eeba5efcf90cf6a639eb5842b8e41e5b1f300b90
André Filipe Silva <andrefilipesilva73@users.noreply.github.com>
2024-01-18 23:36:34 +0000

    Draft implementation of lists on Breeds and Favorites Screens

--- app/src/main/java/com/catcompanion/app/screens/FavoritesListScreen.kt
+++ app/src/main/java/com/catcompanion/app/screens/FavoritesListScreen.kt
@@ -1,31 +1,55 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.catcompanion.app.screens
 
 // Import necessary components from the Jetpack Compose library
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
-// Annotate the function to opt into using ExperimentalMaterial3Api
-@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FavoritesListScreenContent(navController: NavHostController) {
+    val range = 1..100
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(
+            ),
+        // contentPadding = innerPadding,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(range.count()) { index ->
+            Card(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                onClick = {
+                    // Use the navigation controller to navigate to the "breedDetailScreen/{breed}" destination
+                    navController.navigate("breedDetailScreen/{breed}") {
+                        // Configure the navigation behavior
+                        launchSingleTop = true // Launch as a single top-level destination
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true // Save the state of the popped destinations
+                        }
+                    }
+                }
+            ) {
+                // Inside the Card composable, define the content of the card
+                Text(text = "- List item number ${index + 1}")
+            }
+        }
+    }
+}
+
 // Declare a composable function that represents the UI for the com.catcompanion.app.screens.FavoritesListScreen
 @Composable
 fun FavoritesListScreen(navController: NavHostController) {
     // Example of navigating to the detail screen when a card is clicked
-    Card(
-        onClick = {
-            // Use the navigation controller to navigate to the "breedDetailScreen/{breed}" destination
-            navController.navigate("breedDetailScreen/{breed}") {
-                // Configure the navigation behavior
-                launchSingleTop = true // Launch as a single top-level destination
-                popUpTo(navController.graph.startDestinationId) {
-                    saveState = true // Save the state of the popped destinations
-                }
-            }
-        }
-    ) {
-        // Inside the Card composable, define the content of the card
-        Text(text = "Go to Breed Detail from Favorites") // Display a text inside the card
-    }
+    FavoritesListScreenContent(navController)
 }
