f0fd75182ebbf45c41a16f6b8481f5c59decb13d
André Filipe Silva <andrefilipesilva73@users.noreply.github.com>
2024-01-19 15:44:06 +0000

    Simulate a Network request on the repository to discover possible performance issues

--- app/src/main/java/com/catcompanion/app/repository/BreedRepository.kt
+++ app/src/main/java/com/catcompanion/app/repository/BreedRepository.kt
@@ -1,6 +1,7 @@
 package com.catcompanion.app.repository
 
 import com.catcompanion.app.model.Breed
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -15,6 +16,8 @@ class BreedRepository {
 
     suspend fun getBreeds(): Flow<List<Breed>> {
         // Simulate fetching users from a data source (e.g., database)
+        delay(5000)
+
         // Use MutableStateFlow for simplicity, replace with appropriate Flow implementation
         return MutableStateFlow(defaultBreeds.toList())
     }
