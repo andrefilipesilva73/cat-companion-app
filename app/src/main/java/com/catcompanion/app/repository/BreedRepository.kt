543326bb57aa0d348b43b4773339255548653652
André Filipe Silva <andrefilipesilva73@users.noreply.github.com>
2024-01-19 22:03:00 +0000

    Added X-API-Key Header to Cat API Service

--- app/src/main/java/com/catcompanion/app/repository/BreedRepository.kt
+++ app/src/main/java/com/catcompanion/app/repository/BreedRepository.kt
@@ -3,18 +3,34 @@ package com.catcompanion.app.repository
 import com.catcompanion.app.BuildConfig
 import com.catcompanion.app.api.CatApiService
 import com.catcompanion.app.model.Breed
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 class BreedRepository {
     // Cat API Service
     private val catApiService: CatApiService by lazy {
+        // Define the interceptor, add authentication headers
+        val interceptor = Interceptor { chain ->
+            val newRequest: Request =
+                chain.request().newBuilder().addHeader("x-api-key", BuildConfig.CAT_API_API_KEY).build()
+            chain.proceed(newRequest)
+        }
+
+        // Add the interceptor to OkHttpClient
+        val builder = OkHttpClient.Builder()
+        builder.interceptors().add(interceptor)
+        val client = builder.build()
+
         Retrofit.Builder()
-            .baseUrl(BuildConfig.CAT_API_BASE_URL) // Replace with the actual base URL
+            .baseUrl(BuildConfig.CAT_API_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(CatApiService::class.java)
     }
