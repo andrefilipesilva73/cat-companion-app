000e465b9409143048f1e44ff273c9d45f9cca4c
André Filipe Silva <andrefilipesilva73@users.noreply.github.com>
2024-01-22 23:02:29 +0000

    Update HomeScreenTest.kt

--- app/src/androidTest/java/com/catcompanion/app/HomeScreenTest.kt
+++ app/src/androidTest/java/com/catcompanion/app/HomeScreenTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.catcompanion.app.db.AppDatabase
@@ -20,7 +19,6 @@ import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.ByteArrayInputStream
 
 /**
  * Main Activity Instrumented test, which will execute on an Android device.
