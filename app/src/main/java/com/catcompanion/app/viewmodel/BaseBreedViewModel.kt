2c72dddbfc245040f8ef34f85f704082621eb193
André Filipe Silva <andrefilipesilva73@users.noreply.github.com>
2024-01-22 01:57:15 +0000

    Delay Search to allow user to type before execute multiple searches

--- app/src/main/java/com/catcompanion/app/viewmodel/BaseBreedViewModel.kt
+++ app/src/main/java/com/catcompanion/app/viewmodel/BaseBreedViewModel.kt
@@ -6,11 +6,10 @@ import androidx.paging.PagingData
 import androidx.paging.map
 import com.catcompanion.app.model.Breed
 import com.catcompanion.app.repository.BreedRepository
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 abstract class BaseBreedViewModel(private val breedRepository: BreedRepository) : ViewModel() {
@@ -29,28 +28,10 @@ abstract class BaseBreedViewModel(private val breedRepository: BreedRepository) 
 
     // Search list
     private val _searchResultsList = MutableStateFlow<List<Breed>>(emptyList<Breed>())
-    val searchResultsList = searchText
-        .combine(_searchResultsList) { text, _ ->// Combine searchText with _searchResultsList
-            // Evaluate current text
-            if (text.isBlank()) { // Return an empty list (it could be improved with recent breeds or favorites, etc).
-                emptyList<Breed>()
-            } else {
-                try {
-                    // Otherwise, filter and return a list of names based on the text the user typed
-                    breedRepository.getBreedsBySearch(text)
-                } catch (e: Exception) {
-                    // Handle errors (e.g., network issues)
-                    e.printStackTrace()
+    val searchResultsList = _searchResultsList.asStateFlow()
 
-                    // Return an empty list (This should be improved later)
-                    emptyList<Breed>()
-                }
-            }
-        }.stateIn( // Basically convert the Flow returned from combine operator to StateFlow
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5000), // It will allow the StateFlow survive 5 seconds before it been canceled
-            initialValue = _searchResultsList.value
-        )
+    // Hold Search Job
+    private var searchJob: Job? = null
 
     abstract fun fetchData()
 
@@ -59,9 +40,36 @@ abstract class BaseBreedViewModel(private val breedRepository: BreedRepository) 
         fetchData()
     }
 
+    fun performSearch() {
+        viewModelScope.launch {
+            try {
+                // Perform the search using the repository
+                val searchResults = breedRepository.getBreedsBySearch(_searchText.value)
+
+                // Update the search results list
+                _searchResultsList.value = searchResults
+            } catch (e: Exception) {
+                // Handle errors (e.g., network issues)
+                e.printStackTrace()
+
+                // In case of an error, update the search results list with an empty list
+                _searchResultsList.value = emptyList()
+            }
+        }
+    }
+
     fun onQueryChange(text: String) {
         // Set search text
         _searchText.value = text
+
+        // Cancel the previous search job (if any)
+        searchJob?.cancel()
+
+        // Start a new search job with a delay
+        searchJob = viewModelScope.launch {
+            delay(500L)
+            performSearch()
+        }
     }
 
     fun onSearch(text: String) {
@@ -70,6 +78,9 @@ abstract class BaseBreedViewModel(private val breedRepository: BreedRepository) 
 
         // Close Search
         _isSearching.value = !_isSearching.value
+
+        // Perform search
+        performSearch()
     }
 
     fun onToggleSearch() {
