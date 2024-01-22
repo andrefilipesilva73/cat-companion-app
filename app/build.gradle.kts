0e1b4381c01a3df541bbd8d3616fd537b2bad4e3
André Filipe Silva <andrefilipesilva73@users.noreply.github.com>
2024-01-22 03:00:37 +0000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,9 +74,9 @@ android {
 // Dependencies block specifying the project dependencies
 dependencies {
     // Implementation dependencies for various libraries
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
